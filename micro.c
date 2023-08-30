d47b77614fe4da8d914f01922cbfa81e56ff21f1
Atharva Pardeshi <atharva.exe@gmail.com>
2023-08-30 17:14:18 +0530

    refactor: turn off software flow control, allow reading Ctrl-S and Ctrl-Q

--- micro.c
+++ micro.c
@@ -20,6 +20,7 @@ void enableRawMode() {
   // update original attributes, disable echoing input and read input
   // byte-by-byte
   struct termios raw = orig_termios;
+  raw.c_iflag &= ~(IXON);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
