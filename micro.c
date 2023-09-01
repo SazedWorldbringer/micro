2b224b4f891c3f0fe7269601f05aac80208e8a3d
Atharva Pardeshi <atharva.exe@gmail.com>
2023-09-01 23:33:12 +0530

    refactor: display 13 when Ctrl-m is pressed

--- micro.c
+++ micro.c
@@ -20,7 +20,7 @@ void enableRawMode() {
   // update original attributes, disable echoing input and read input
   // byte-by-byte
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(IXON);
+  raw.c_iflag &= ~(ICRNL | IXON);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
