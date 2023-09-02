923147dd0fea3cadec76e2a77177aeedbfbe0d9c
Atharva Pardeshi <atharva.exe@gmail.com>
2023-09-02 22:48:13 +0530

    refactor: turn off output processing, add carriage return manually

--- micro.c
+++ micro.c
@@ -21,6 +21,7 @@ void enableRawMode() {
   // byte-by-byte
   struct termios raw = orig_termios;
   raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -34,10 +35,10 @@ int main() {
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
     if (iscntrl(c)) {
       // print the ascii code of the character pressed
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
       // print the character it represents if it isn't a control character
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
 
