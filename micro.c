52960eddf9202f09116be05f1e05423e39b47a60
Atharva Pardeshi <atharva.exe@gmail.com>
2023-09-02 23:32:00 +0530

    feat: return read after every 10th of a second

--- micro.c
+++ micro.c
@@ -22,7 +22,10 @@ void enableRawMode() {
   struct termios raw = orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag &= ~(CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -31,8 +34,9 @@ int main() {
   enableRawMode();
 
   // read input from the terminal
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) {
       // print the ascii code of the character pressed
       printf("%d\r\n", c);
@@ -40,6 +44,7 @@ int main() {
       // print the character it represents if it isn't a control character
       printf("%d ('%c')\r\n", c, c);
     }
+    if(c == 'q') break;
   }
 
   return 0;
