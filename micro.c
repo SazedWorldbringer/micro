9429fcf72761acfaab987bf5563344e613d0e8b3
Atharva Pardeshi <atharva.exe@gmail.com>
2023-09-02 22:57:56 +0530

    chore: turn off miscellaneous flags

--- micro.c
+++ micro.c
@@ -20,7 +20,7 @@ void enableRawMode() {
   // update original attributes, disable echoing input and read input
   // byte-by-byte
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
